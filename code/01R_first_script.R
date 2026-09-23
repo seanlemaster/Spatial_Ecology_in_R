bd5f7cf7af4c2fe483eb591ab87c67d6499a7720
seanlemaster <seanlemaster@mail.com>
2026-09-23 10:40:44 +0200

    Enhance R script with operations and assignments
    Added operations and object assignments in R script.

--- code/01R_first_script.R
+++ code/01R_first_script.R
@@ -1,6 +1,20 @@
+# Script for using R.
 
-
+# Operation.
 2 + 3
 
-
+# Assigning an object.
 samuele <- 2 + 3
+
+# Another object.
+gemma <- 4 + 6
+
+# Different Objects.
+samuele + gemma
+samuele * gemma
+samuele ^ gemma
+
+
+tia <- 5 * 4
+
+tia + samuele + gemma
